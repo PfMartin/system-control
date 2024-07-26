13a0aef4c9c1484b679a6224cec9986fca1b864b
PfMartin <martinpfatrisch@gmail.com>
2024-07-26 21:45:56 +0200

    Basic layout for navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,19 @@
 <template>
-  <p>Navigation</p>
-  <ul>
-    <li>
-      <NuxtLink to="/home">Home</NuxtLink>
-    </li>
-    <li>
-      <NuxtLink to="/about">About</NuxtLink>
-    </li>
-  </ul>
+  <div class="flex justify-between w-screen bg-gray-700">
+    <div class="flex">
+      <div id="logo" class="bg-gray-900 text-white p-3">Logo</div>
+
+      <ul id="nav" class="flex items-center bg-blue-200 list-none p-0 m-0">
+        <li class="p-2 m-1 hover:bg-indigo-500 rounded">
+          <NuxtLink to="/home">Home</NuxtLink>
+        </li>
+        <li class="p-2 m-1 hover:bg-indigo-500 rounded">
+          <NuxtLink to="/devices">Devices</NuxtLink>
+        </li>
+      </ul>
+    </div>
+
+    <div id="search" class="flex items-center">Search</div>
+  </div>
   <slot></slot>
 </template>
